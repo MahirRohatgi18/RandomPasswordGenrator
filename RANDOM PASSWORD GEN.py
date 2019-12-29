f2952754eb26d863c9d045cc68fdf556820a5cd7
MahirRohatgi18 <59334324+MahirRohatgi18@users.noreply.github.com>
2019-12-29 17:40:41 +0530

    Add files via upload

---
+++ RANDOM PASSWORD GEN.py
@@ -0,0 +1,38 @@
+import random # imports random module 
+print("Do you want to Genrate a random password ? type Yes or No")
+a = input("Enter Your Choice  :  ").capitalize()
+#a = a.capitalize()
+if a== 'Yes' :
+    length = int(input("Enter the length of the password : "))
+    print ("Genrating your Password")
+    print ("Your Password is : ")
+    # code for password
+    pw= str() #empty string so it can be incremented later
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ1234567890!@#$%^&*()_=+-₹'
+    #characters are defined
+    for i in range (length): #goes through all possible permutations
+        pw = pw + random.choice(characters) #adds a random choice to the string at every instance of i
+    print(pw) # prints final pw
+    
+elif a=='No':
+    print ("Have a nice day!")
+    
+else :
+    print() 
+    print('CHOOSE BETWEEN Yes AND No! ')
+    print()
+    print("Genrate password ? type Yes or No")
+    a = input("Enter Your Choice  :  ").capitalize()
+#a = a.capitalize()
+    #see explantion in the original if statement
+    if a== 'Yes' :
+       length = int(input("Enter the length of the password : "))
+    print ("Genrating your Password")
+    print ("Your Password is : ")
+    # code for password
+    pw= str()
+    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ1234567890!@#$%^&*()_=+-₹'
+    for i in range (length):
+        pw = pw + random.choice(characters)
+    print(pw)
+      
