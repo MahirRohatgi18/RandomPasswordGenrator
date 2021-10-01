a49f9a6cacfa807b421316ec7ebda64c2c17ab0d
Mahir <59334324+MahirRohatgi18@users.noreply.github.com>
2021-10-01 19:42:26 +0530

    Update RANDOM PASSWORD GEN.py

--- RANDOM PASSWORD GEN.py
+++ RANDOM PASSWORD GEN.py
@@ -21,30 +21,4 @@ if a== 'Yes' :
     saveFile.close() #file is saved where the main project is 
     
 elif a=='No':
-    print ("Have a nice day!")
-    
-else :
-    print() 
-    print('CHOOSE BETWEEN Yes AND No! ')
-    print()
-    print("Genrate password ? type Yes or No")
-    a = input("Enter Your Choice  :  ").capitalize()
-#a = a.capitalize()
-    #see explantion in the original if statement
-    if a== 'Yes' :
-       length = int(input("Enter the length of the password : "))
-    print ("Genrating your Password")
-    print ("Your Password is : ")
-    # code for password
-    pw= str()
-    characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWQYZ1234567890!@#$%^&*()_=+-₹'
-    for i in range (length):
-        pw = pw + random.choice(characters)
-    print(pw)
-    
-     # code for saving file :
-    password = (pw)
-    saveFile = open('RandomPassword.txt','x') # RandomPassword is file name 
-    saveFile.write(str(password))
-    saveFile.close() #file is saved where the main project is 
-      
+    print ("Have a nice day!")       
